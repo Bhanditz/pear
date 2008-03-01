b3cd4ce4d9b36069741eaa01c3bc15ee18615aec
Mehmet Balcilar <mehmet@mbalcilar.net>
2008-03-01 00:00:00 +0000

    version 1.0

---
+++ R/peboxplot.R
@@ -0,0 +1,13 @@
+`peboxplot` <-
+function(z, ...)
+{
+	ztitle <- attr(z, "title")
+	z.names <- attr(z, "period.abb")
+	if(is.null(z.names))
+		z.names <- as.character(unique(cycle(z)))
+	boxplot(split(z, cycle(z)), names = z.names, ...)
+	if(!is.null(ztitle))
+		title(main = ztitle)
+	NULL
+}
+
